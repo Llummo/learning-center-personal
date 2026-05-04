095f9bf0245b29f735ed60b50f3f952430e97186
Llummo <167897805+Llummo@users.noreply.github.com>
2026-05-04 09:21:26 -0500

    chore: fixed providers

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import {provideRouter} from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideHttpClient(),
+        provideTranslateService({ defaultLanguage: 'en' }),
+        provideRouter([]),
+      ],
     }).compileComponents();
   });
 
@@ -18,6 +26,9 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, learning-center');
+    expect(compiled.querySelector('h1')?.textContent).toContain('ACME Learning Center');
   });
+
+
+
 });
